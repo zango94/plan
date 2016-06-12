dadcae3fecee2548eaeee1497b46fd936164eb4d
zango94 <ddwywort@gmail.com>
2016-06-12 23:18:10 +0200

    praca

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php session_start();
 mysql_connect("localhost","root","") or die(mysql_error()."Nie mozna polaczyc sie z baza danych. Prosze chwile odczekac i sprobowac ponownie.");
 mysql_select_db("plan") or die(mysql_error()."Nie mozna wybrac bazy danych.");
+// połączenie i wybranie bazy danych
 ?>
