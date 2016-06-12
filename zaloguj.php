270d2412c06b79612f299a78dc80ff180aa7ed12
zango94 <ddwywort@gmail.com>
2016-06-12 23:57:57 +0200

    Praca

--- zaloguj.php
+++ zaloguj.php
@@ -13,32 +13,23 @@ if ($_GET['haslo'] != '') { //jezeli ktos przez adres probuje kombinowac
 exit;
 }
 
-//$haslo = md5($haslo); //szyfrowanie hasla
     if (!$login OR empty($login)) {
-//include("head2.php");
 	$host  = $_SERVER['HTTP_HOST'];
 	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 	$extra = 'logowanie.php?error=1';
 	header("Location: http://$host$uri/$extra");
-//echo '<p class="alert">Wypełnij pole z loginem!</p>';
-//include("foot.php");
 exit;
 }
     if (!$haslo OR empty($haslo)) {
-//include("head2.php");
 $host  = $_SERVER['HTTP_HOST'];
 	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 	$extra = 'logowanie.php?error=2';
 	header("Location: http://$host$uri/$extra");
-//echo '<p class="alert">Wypełnij pole z hasłem!</p>';
-//include("foot.php");
 exit;
 }
 
-
 $istnick = mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM `uzytkownicy` WHERE `nick` = '$login' AND `haslo` = '$haslo'")); // sprawdzenie czy istnieje uzytkownik o takim nicku i hasle
-    if ($istnick[0] == 0) {
-//echo 'Logowanie nieudane. Sprawdź pisownię nicku oraz hasła.';
+    if ($istnick[0] == 0) {;
 $host  = $_SERVER['HTTP_HOST'];
 	$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 	$extra = 'logowanie.php?error=3';
